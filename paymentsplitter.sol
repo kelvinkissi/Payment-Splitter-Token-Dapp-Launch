e4605ffebf025471756ae42906c9bda1581dc43b
kelvin kissi <87921424+kissikelvin@users.noreply.github.com>
2022-09-08 04:01:00 -0700

    Update paymentsplitter.sol

--- paymentsplitter.sol
+++ paymentsplitter.sol
@@ -1,14 +1,16 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
-// Imported the Openzeppelin library/finance/PaymentSplitter
+// Imported Repo and 2 different the Openzeppelin libraries/finance/PaymentSplitter "../utils/address.sol" & "../utils/context.sol"
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/finance/PaymentSplitter.sol";
-// Named contract "PAYMENTS"
+// Named contract "PAYMENTS" to avoid confusion and inherited from PaymentSplitter contract 
 contract PAYMENTS is PaymentSplitter {
 
     constructor (address[] memory _payees, uint256[] memory _shares) PaymentSplitter(_payees, _shares) payable {}
 
 }
+
+// Addresses & Distribution used in the DEPLOY  _PAYEES, _SHARES after deploying our new "PAYMENTS" contract. 
 /**
     ["0x82D1551fa77A238D62F137BD3088B033602EDf15", //Investor #1
     "0x885912062F0Cb1F1B4A8ff428e90AB1d4C58b294",  //Investor #2
@@ -20,3 +22,8 @@ contract PAYMENTS is PaymentSplitter {
     20,  // "20%" distribution
     10] // "10%" distribution
 */
+// Check the totalShares in the Deployed Contracts to insure that the total distribution is 100%
+// Copy the deployed "PAYMENTS" contract address and used it to deploy the "TICKETS_SALES_PAYMENTS" contract
+// Added _NAME _SYMBOL _INITBASEURI _PAYMENTS: "PAYMENTS" contract address
+// Used owners address of the deployed "TICKETS_SALES_PAYMENTS" contract to withdraw the funds into the "PAYMENTS" contract
+// Used the designated investor address to release the share profits
